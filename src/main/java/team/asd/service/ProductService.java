7f0781963a32eae64153d1a282cc29f94a62dd17
Sem4enko <sem4enko_max@ukr.net>
2022-10-21 16:37:08 +0300

    #3 added ProductService

--- src/main/java/team/asd/service/ProductService.java
+++ src/main/java/team/asd/service/ProductService.java
@@ -1,20 +1,31 @@
 package team.asd.service;
 
 import lombok.NonNull;
+import org.apache.commons.collections4.CollectionUtils;
 import team.asd.constants.ProductState;
 import team.asd.entities.IsProduct;
 import team.asd.exceptions.WrongProductException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class ProductService implements IsProductService{
+public class ProductService implements IsProductService {
     @Override
     public @NonNull List<String> defineProductNames(List<IsProduct> productList) throws WrongProductException {
+        if (CollectionUtils.isEmpty(productList)) {
+            return Collections.emptyList();
+        }
+        for (int i = 0; i < productList.size(); i++) {
+            if (productList.get(i) == null) {
+                throw new WrongProductException("Product cannot be null");
+            }
+        }
 
-
-        return  productList.stream()
+        return productList.stream()
+                .filter(Objects::nonNull)
                 .map(IsProduct::getName)
                 .collect(Collectors.toList());
     }
@@ -22,6 +33,10 @@ public class ProductService implements IsProductService{
     @Override
     public List<IsProduct> defineProductsWithCreatedState(List<IsProduct> productList) {
         return null;
+//        return  productList.stream()
+//                .filter(Objects::nonNull)
+//                .filter(e -> e.getState().equals("Created"))
+//                .collect(Collectors.toList());
     }
 
     @Override
