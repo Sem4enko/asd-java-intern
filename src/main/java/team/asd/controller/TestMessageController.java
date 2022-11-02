4ff9a0ed80f7b841f66a32eb459bffeda53a2716
Sem4enko <sem4enko_max@ukr.net>
2022-11-02 14:50:02 +0200

    #23 Implemented TestMessageController

--- src/main/java/team/asd/controller/TestMessageController.java
+++ src/main/java/team/asd/controller/TestMessageController.java
@@ -1,6 +1,8 @@
 package team.asd.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,8 @@ import java.time.LocalDate;
 
 @RestController
 public class TestMessageController {
-
 	@GetMapping(value = "/test/message", produces = MediaType.APPLICATION_JSON_VALUE)
-	public TestMessage message() {
-		return new TestMessage(LocalDate.now(), "Test message from Spring Boot Application!");
+	public ResponseEntity<TestMessage> message() {
+		return new ResponseEntity<>(new TestMessage(LocalDate.now(), "Test message from Spring Boot Application!"), HttpStatus.OK);
 	}
 }
